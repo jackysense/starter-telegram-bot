c57e51c8c3e0cdab73f9d677e372e487eb71d407
Jacky <jackysense8@gmail.com>
2023-08-12 23:44:31 +0800

    update passkeys

--- src/bot.ts
+++ src/bot.ts
@@ -13,11 +13,21 @@ const bot = new Bot(
 // Handle the /yo command to greet the user
 
 const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link1 = "https://example.hanko.io/";
+
+
 
 bot.command("aa", (ctx) => {
   ctx.reply(`Welcome ${ctx.from?.username}`, {
     reply_markup: {
       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  });
+});
+bot.command("key", (ctx) => {
+  ctx.reply(`Welcome ${ctx.from?.username}`, {
+    reply_markup: {
+      keyboard: [[{ text: "passkeys", web_app: { url: web_link1 } }]],
     },
   });
 });
@@ -172,11 +182,12 @@ const aboutUrlKeyboard = new InlineKeyboard().url(
 
 // Suggest commands in the menu
 bot.api.setMyCommands([
-  { command: "aa", description: "Be greeted by the bot" },
-  {
-    command: "effect",
-    description: "Apply text effects on the text. (usage: /effect [text])",
-  },
+  { command: "aa", description: "web app by the bot" },
+  { command: "key", description: "passkeys demo by the bot" },
+  // {
+  //   command: "effect",
+  //   description: "Apply text effects on the text. (usage: /effect [text])",
+  // },
 ]);
 
 // Handle all other messages and the /start command
@@ -184,7 +195,8 @@ const introductionMessage = `Hello! I'm a Telegram bot.
 I'm powered by Cyclic, the next-generation serverless computing platform.
 
 <b>Commands</b>
-/aa - Be greeted by me
+/aa - web app test
+/key - passkeys demo
 /effect [text] - Show a keyboard to apply text effects to [text]`;
 
 const replyWithIntro = (ctx: any) =>
